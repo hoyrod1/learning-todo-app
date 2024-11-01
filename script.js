ed2d8343badfcdaf666fc4726a97d1cae4893aba
Rodney St Cloud <hoyrod1@aol.com>
2024-10-31 20:51:08 -0400

    Set up the ininitial REACT learrning-todo-react-app project folder in the learning-todo-app folder

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log(`Sanity Check For script.js File`);
+// console.log(`Sanity Check For script.js File`);
 //=============================== CACHE THE ARRAY ELEMENTS OF CATEGORIES ===============================//
 const CATEGORIES = [
   { name: "technology", color: "#3b82f6" },
@@ -10,6 +10,8 @@ const CATEGORIES = [
   { name: "history", color: "#f97316" },
   { name: "news", color: "#8b5cf6" },
 ];
+// const testObj = CATEGORIES.find((cat) => cat.name === "science");
+// console.log(testObj.color);
 //================================== CACHE THE ARRAY ELEMENTS OF DATA ==================================//
 const initialFacts = [
   {
@@ -55,19 +57,43 @@ const formBtn = document.getElementById("btn-open");
 //====================== CACHE THE UL ELEMENT ======================//
 const ulList = document.querySelector(".fact-list");
 // console.dir(ulList);
+ulList.innerHTML = "";
+// ulList.insertAdjacentHTML();
+//====================== CACHE THE UL ELEMENT ======================//
+const ulCatList = document.querySelector(".category-list");
+// console.dir(ulCatList);
+ulCatList.innerHTML = "";
 //==================================================================//
 
 //==================================================================//
 // CREATE DOM ELEMENTS AND RENDER FACTS IN THE LI ELEMENT
 //------------------------------------------------------------------//
-ulList.innerHTML = "";
-// ulList.insertAdjacentHTML();
+// FETCH FACTS DATA FROM THE Superbase API
+async function loadApiFacts() {
+  const res = await fetch(
+    "https://wdmryufnequrkcumvggg.supabase.co/rest/v1/technolgy-todo",
+    {
+      headers: {
+        apikey:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndkbXJ5dWZuZXF1cmtjdW12Z2dnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk0ODI1NzgsImV4cCI6MjA0NTA1ODU3OH0.3P-wZ1jCSsyZQ-ycxbfjaq49JuGX1jo6H2D9Q_vM_Dg",
+        authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndkbXJ5dWZuZXF1cmtjdW12Z2dnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk0ODI1NzgsImV4cCI6MjA0NTA1ODU3OH0.3P-wZ1jCSsyZQ-ycxbfjaq49JuGX1jo6H2D9Q_vM_Dg",
+      },
+    }
+  );
+  const data = await res.json();
+  // console.log(data);
+  //------------------------------------------------------------------//
+  // CALLING THE FUNCTION TO RENDER THE FACTS DATA IN THE LI ELEMENT
+  creatFactsList(data);
+}
 //------------------------------------------------------------------//
-// CALLING THE FUNCTION TO RENDER THE DOM ELEMENTS OF LI's
-creatFactsList(initialFacts);
+loadApiFacts();
 //------------------------------------------------------------------//
+//======================================================================//
+
+//======================================================================//
 // CREATE A REUSABLE FUNCTION COMPONENT THAT REPRESENT THE LI ELEMENT
-//------------------------------------------------------------------//
 function creatFactsList(arrayOfData) {
   const htmlLiArray = arrayOfData.map(
     (fact) => `<li class="fact"> 
@@ -78,12 +104,14 @@ function creatFactsList(arrayOfData) {
                     target="_blank>"
                     >(source)</a> 
                   </P>
-                  <span class="tag" style="background-color: #3b82f6"
+                  <span class="tag" style="background-color: ${
+                    CATEGORIES.find((cat) => cat.name === fact.category).color
+                  }"
                     >${fact.category}</span
                   >
                   <div class="vote-buttons">
                     <button>👍🏾 ${fact.votesInteresting}</button>
-                    <button>🤯 ${fact.votesMindblowing}</button>
+                    <button>🤯 ${fact.votesMindBlowing}</button>
                     <button>⛔️ ${fact.votesFalse}</button>
                   </div>
                </li>`
@@ -91,12 +119,36 @@ function creatFactsList(arrayOfData) {
   //------------------------------------------------------------------//
   const htmlLiEl = htmlLiArray.join("");
   //------------------------------------------------------------------//
-  // APPEND THE MAPPED LI ELEMENT TO THE UL ELEMENT
+  // APPEND THE MAPPED LI ELEMENT TO THE UL ELEMENT FOR FACTS
   ulList.insertAdjacentHTML("afterbegin", htmlLiEl);
-  //==================================================================//
+  //------------------------------------------------------------------//
 }
+//======================================================================//
 
-//==================================================================//
+//======================================================================//
+// CREATE A REUSABLE FUNCTION COMPONENT THAT REPRESENT THE LI ELEMENT
+function creatCategoryList(arrayOfCategories) {
+  const htmlLiArray = arrayOfCategories.map(
+    (category) => `<li class="category">
+                      <button
+                        class="btn btn-category"
+                        style="background-color: ${category.color}"
+                      >
+                        ${category.name}
+                      </button>
+                   </li>`
+  );
+  //------------------------------------------------------------------//
+  const htmlLiEl = htmlLiArray.join("");
+  //------------------------------------------------------------------//
+  // APPEND THE MAPPED LI ELEMENT TO THE UL ELEMENT FOR CATEGORIES
+  ulCatList.insertAdjacentHTML("afterbegin", htmlLiEl);
+  //------------------------------------------------------------------//
+}
+creatCategoryList(CATEGORIES);
+//======================================================================//
+
+//======================================================================//
 // CREATE AN EVENT LISTENER FOR TO TOGGLE SHARE FACT BUTTON ELEMENT
 formBtn.addEventListener("click", function (e) {
   if (form.classList.contains("hidden")) {
@@ -107,6 +159,7 @@ formBtn.addEventListener("click", function (e) {
     formBtn.textContent = "Share a fact";
   }
 });
+//======================================================================//
 
 //== SET THE VOTING VARIABLES ==//
 let votesInteresting = 0;
