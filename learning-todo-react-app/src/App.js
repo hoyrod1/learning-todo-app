aa936f1d1f87543eb2a480a966a8c250df127cb5
Rodney St Cloud <hoyrod1@aol.com>
2024-10-31 21:48:18 -0400

    Added the style.css file to the public directory. added the header element to the App component and imported the style.css to the App.js file

--- learning-todo-react-app/src/App.js
+++ learning-todo-react-app/src/App.js
@@ -1,5 +1,17 @@
+import "./style.css";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="logo.png" alt="Today I Learned Logo" />
+        <h1>Today I Learned</h1>
+      </div>
+      <button id="btn-open" className="btn btn-large">
+        Share A Fact
+      </button>
+    </header>
+  );
 }
 
 export default App;
